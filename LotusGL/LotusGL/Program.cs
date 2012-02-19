3395d5b89b8183a0dbf1af4c468f35c89af1d9e3
Daniel Sont <dan.sont@gmail.com>
2012-02-19 18:29:49 -0500

    First Commit

---
+++ LotusGL/LotusGL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+namespace LotusGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow window = new OpenTK.GameWindow(500, 500);
+            window.Run(60);
+            window.RenderFrame
+        }
+    }
+}
